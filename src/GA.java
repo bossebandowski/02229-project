67c6eaf8105c55644ad13d876fce794bd5889945
Szabolcs <gardas86@gmail.com>
2020-11-10 01:37:06 +0100

    Genetic algorithm class

---
+++ src/GA.java
@@ -0,0 +1,183 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class GA {
+    ArrayList<Stream> _streams;
+    int _init_population_size;
+    int _population_size;
+    int _number_of_generations;
+    int _number_of_children;
+    HashMap<Stream,List<List<Integer>>> _population;
+    HashMap<Stream,List<Float>> _fitness; // Fitness level of each member of population
+    Random rand = new Random();
+
+    public GA(ArrayList<Stream> streams, int init_population_size, int _population_size,
+              int _number_of_generations, int _number_of_children)
+    {
+        this._streams = streams;
+        this._init_population_size = init_population_size;
+        this._population_size = _population_size;
+        this._number_of_generations = _number_of_generations;
+        this._number_of_children = _number_of_children;
+    }
+
+    public void initPopulation()
+    {
+        for(Stream currentStream:_streams)
+        {
+            List<List<Integer>> generated_population = new ArrayList<>();
+            for(int i = 0; i < _init_population_size; i++)
+            {
+                //generated_population.add(...);
+                //TODO: generate random routes between the start and end nodes
+            }
+            _population.put(currentStream,generated_population);
+        }
+    }
+
+    public void calculateFitness()
+    {
+        for(Stream currentStream: _streams)
+        {
+            _fitness.get(currentStream).clear();
+            List<List<Integer>> currentPopulation = _population.get(currentStream);
+            for(List<Integer> currentSolution:currentPopulation)
+            {
+                _fitness.get(currentStream).add(calculateSolutionFitness(currentSolution));
+            }
+        }
+    }
+
+    public float calculateSolutionFitness(List<Integer> solution)
+    {
+        float result = 0.0f;
+        // TODO calculate fitness
+        return result;
+    }
+    public HashMap<Stream,List<List<Integer>>> selectParents(int number_of_pairs)
+    {
+        HashMap<Stream,List<List<Integer>>> result = new HashMap<>(); // number_of_pairs * 2 solution assigned to each streams
+        // Roulette-wheel selection
+        for(Stream currentStream: _streams)
+        {
+            List<List<Integer>> currentPopulation = _population.get(currentStream);
+            List<Float> currentFitness = _fitness.get(currentStream);
+            List<Float> probability = new ArrayList<>();
+            List<Float> cumulativeProbability = new ArrayList<>();
+            List<List<Integer>> selectedParents = new ArrayList<>();
+            float sumFitness = sumFitness(currentStream);
+            for(Float f:currentFitness)
+            {
+                probability.add(f / sumFitness);
+            }
+            float probabilityBuffer = 0.0f;
+            for(Float f:probability)
+            {
+                probabilityBuffer += f;
+                cumulativeProbability.add(probabilityBuffer);
+            }
+
+            for(int i = 0; i < 2 * number_of_pairs; i++)
+            {
+                float rand1 =  (float)rand.nextDouble();
+                System.out.println(rand1);
+                if(cumulativeProbability.get(0) > rand1)
+                {
+                    selectedParents.add(currentPopulation.get(0));
+                }
+                else
+                {
+                    for(int k = 1; k < cumulativeProbability.size(); k++)
+                    {
+                        if(cumulativeProbability.get(k) > rand1)
+                        {
+                            if(k + 1 < cumulativeProbability.size())
+                            {
+                                selectedParents.add(currentPopulation.get(k+1));
+                            }
+                            else
+                            {
+                                selectedParents.add(currentPopulation.get(k));
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return  result;
+    }
+
+    public float sumFitness(Stream stream)
+    {
+        float result = 0.0f;
+        for(float f:_fitness.get(stream))
+        {
+            result += f;
+        }
+        return result;
+    }
+
+
+    public List<List<Integer>> generateGeneration()
+    {
+        List<List<Integer>> newGeneration = new ArrayList<>();
+        HashMap<Stream,List<List<Integer>>> parents = selectParents(_number_of_children);
+        for(Stream currentStream: _streams)
+        {
+            List<List<Integer>> currentParents = parents.get(currentStream);
+            int parentPointer = 0;
+            for(int i = 0; i < _number_of_children; i++)
+            {
+                int rand1 = rand.nextInt(1) + parentPointer;
+                int rand2 = 0;
+                if(rand1 == 0)
+                {
+                    rand2 = 1;
+                }
+                changeRandomSegment(currentParents.get(parentPointer+rand1),currentParents.get(parentPointer+rand2));
+                parentPointer += 2;
+            }
+        }
+        return newGeneration;
+    }
+
+    public List<List<Integer>> changeRandomSegment(List<Integer> parent1, List<Integer> parent2)
+    {
+        List<List<Integer>> mergedRoute = new ArrayList<>();
+        boolean foundCommonNode = false;
+        while(!foundCommonNode) {
+            int nodeToChangeIndex1 = rand.nextInt(parent1.size() - 2) + 1;
+            int nodeToChangeIndex2 = 0;
+            int nodeToChange = parent1.get(nodeToChangeIndex1);
+            for (int i = 0; i < parent2.size(); i++) {
+                if (parent2.get(i) == nodeToChange) {
+                    nodeToChangeIndex2 = i;
+                    foundCommonNode = true;
+                }
+            }
+        }
+            // found another common node
+            /*int returnIndex1 = 0;
+            int returnIndex2 = 0;
+            for(int i = nodeToChangeIndex1 + 1; i < parent1.size(); i++)
+            {
+                int currentNodeIndex1 = parent1.get(i);
+                for(int k = nodeToChangeIndex2; k < parent2.size(); k++)
+                {
+                    if(currentNodeIndex1 == parent2.get(k))
+                    {
+                        // In worst case this is the destination node
+                        returnIndex1 = i;
+                        returnIndex2 = k;
+                    }
+                }
+            }
+            for(int i = nodeToChangeIndex1)*/
+        return  mergedRoute;
+    }
+
+
+}
